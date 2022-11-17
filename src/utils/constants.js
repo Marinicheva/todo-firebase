0583951b1637c73e2ec5e8eacc1e548b9d9a292e
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-11-17 23:04:22 +0300

    feat: prepare to work

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjk3s8XGOaUrjIzPYUegDNufXqtBU-fZA",
+  authDomain: "todo-4b4b3.firebaseapp.com",
+  databaseURL: "https://todo-4b4b3-default-rtdb.firebaseio.com",
+  projectId: "todo-4b4b3",
+  storageBucket: "todo-4b4b3.appspot.com",
+  messagingSenderId: "8571773295",
+  appId: "1:8571773295:web:a63d73a2ae4b7d83dc889a"
+};
